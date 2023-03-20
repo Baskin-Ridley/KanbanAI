c627db34eda0bf527f4a48135295416531ae5c6a
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-20 15:27:06 +0000

    basic styling

--- client/src/components/Kanban/TaskHeaders/index.jsx
+++ client/src/components/Kanban/TaskHeaders/index.jsx
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
+import { DndContext, useDraggable } from "@dnd-kit/core";
 
 const TaskHeader = () => {
-  return <div>Test</div>;
+  const [testHeaders, setTestHeaders] = useState([
+    {
+      id: 1,
+      title: "To Do",
+      color: "bg-red-500",
+    },
+    {
+      id: 2,
+      title: "In Progress",
+      color: "bg-blue-500",
+    },
+    {
+      id: 3,
+      title: "Done",
+      color: "bg-green-500",
+    },
+  ]);
+
+  return (
+    <div className="flex space-x-4">
+      {testHeaders.map((header) => (
+        <div
+          key={header.id}
+          className={`p-4 rounded-md ${header.color} text-white font-medium`}
+        >
+          {header.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TaskHeader;
--- client/src/components/Kanban/TaskTickets/index.jsx
+++ client/src/components/Kanban/TaskTickets/index.jsx
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
 
 const TaskTickets = () => {
-  return <div>Test</div>;
+  const [testTickets, setTestTickets] = useState([
+    {
+      id: 1,
+      title: "Test Ticket 1",
+      description: "This is a test ticket",
+    },
+    {
+      id: 2,
+      title: "Test Ticket 2",
+      description: "This is a test ticket",
+    },
+    {
+      id: 3,
+      title: "Test Ticket 3",
+      description: "This is a test ticket",
+    },
+  ]);
+  return (
+    <div>
+      {testTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h3>{ticket.title}</h3>
+          <p>{ticket.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TaskTickets;
