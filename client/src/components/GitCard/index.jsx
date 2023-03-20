361355dc600b056ee525f42988be6ad7e518f68a
shojib deb <shodeb123@gmail.com>
2023-03-20 17:45:39 +0000

    fixed where git commit has no user

--- client/src/components/GitCard/index.jsx
+++ client/src/components/GitCard/index.jsx
@@ -6,7 +6,11 @@ function GitCommitCard({ data }) {
     return (
         <>
             {Object.keys(data).map((e, i) => (
-                <li key={i}>author: {data[e].author.login} <br /> commit: {data[e].commit.message}<br /> at: {data[e].commit.author.date.slice(11, 16)} day: {data[e].commit.author.date.slice(0, 10)} </li>
+                <div className='gitCard'>
+                    <li key={i}> author: {data[e].author ? data[e].author.login : "System"} </li>
+                    <li> commit: {data[e].commit.message}</li>
+                    <li>  at: {data[e].commit.author.date.slice(11, 16)} day: {data[e].commit.author.date.slice(0, 10)} </li>
+                </div>
             ))}
         </>
     )
