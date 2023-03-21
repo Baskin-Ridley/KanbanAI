cb553ea7279e3f0c5f74e9a83d242b5244b9bf1c
shojib deb <shodeb123@gmail.com>
2023-03-21 10:42:30 +0000

    gitCard functionalities

--- client/src/pages/Github/index.jsx
+++ client/src/pages/Github/index.jsx
@@ -4,13 +4,30 @@ import "./style.css"
 
 function GitHubComments() {
 
-    const [commits, setCommits] = useState()
+    const [commits, setCommits] = useState(undefined)
+    const [states, setStates] = useState(false)
+    const [user, setUser] = useState("")
+    const [repo, setRepo] = useState("")
+
+
+    const clickHandler = (states) => {
+        setStates(!states)
+    }
+
+    const nameHandler = (e) => {
+        setUser(e.target.value)
+
+
+    }
+    const repoHandler = (e) => {
+        setRepo(e.target.value)
+    }
 
     useEffect(() => {
 
         async function loadCommits() {
             try {
-                const response = await fetch("https://api.github.com/repos/Baskin-Ridley/final-project/commits")
+                const response = await fetch(`https://api.github.com/repos/${user}/${repo}/commits`)
                 const data = await response.json();
                 setCommits(data)
             } catch (error) {
@@ -18,14 +35,30 @@ function GitHubComments() {
             }
         }
         loadCommits()
-    }, [])
+    }, [states])
+
+
 
     return (
 
         <>
-            <ul>
-                {commits && <GitCommitCard data={commits} />}
-            </ul>
+            <form>
+                <label>
+                    <div className='input-section'>
+                        Name:
+                        <input className='input-line' type="text" name="name" onChange={nameHandler} />
+                    </div>
+
+                </label>
+                <label>
+                    <div className='input-section'>
+                        repository:
+                        <input className='input-line' type="text" name="repository" onChange={repoHandler} />
+                    </div>
+                </label>
+            </form>
+            <button className='access-gitcard-btn' onClick={() => { clickHandler(states) }}>Click me to get the commitCards</button>
+            {states ? <ul>{commits && <GitCommitCard data={commits} />}</ul> : null}
         </>
     )
 }
