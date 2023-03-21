6bce5119773a4a6fc28e2b6b74ca382c742e8aaa
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-21 17:13:35 +0000

    fix bugs

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { UserContext } from '../../context/UserContext';
-import Message from '../../components/message';
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import Message from "../../components/Message";
 
 const LoginPage = () => {
   const { login } = useContext(UserContext);
   const navigate = useNavigate();
-  
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await login({ username, password });
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (err) {
-      setError('Invalid username or password');
+      setError("Invalid username or password");
     }
   };
 
@@ -27,11 +27,23 @@ const LoginPage = () => {
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Login</button>
         <Link to="/register">Don't have an account? Register here.</Link>
