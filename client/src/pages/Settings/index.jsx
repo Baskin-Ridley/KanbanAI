021a2d1d922a8e3b9f5a949c7f0c967d8c030085
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-22 17:05:30 +0000

    Update pages/Settings/index.jsx - fixed bug

--- client/src/pages/Settings/index.jsx
+++ client/src/pages/Settings/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { UserContext } from '../../contexts/UserContext';
+import { UserContext } from '../../context/UserContext';
 import Message from '../../components/Message';
 
 const Settings = () => {
