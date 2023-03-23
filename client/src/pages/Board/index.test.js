1111c16a9588d6c1751fd1771fc3f60b715b8f8a
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-23 12:28:12 +0000

    Create pages/Board/index.test.js

---
+++ client/src/pages/Board/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useParams } from 'react-router-dom';
+import Board from './index.jsx';
+
+jest.mock('react-router-dom', () => ({
+  useParams: jest.fn(),
+}));
+
+jest.mock('../../components/Board', () => ({
+  Headers: jest.fn(() => <div>Mocked Headers</div>),
+}));
+
+describe('Board', () => {
+  test('renders without error', () => {
+    useParams.mockReturnValue({ id: 'mockedId' });
+    render(<Board />);
+  });
+});
