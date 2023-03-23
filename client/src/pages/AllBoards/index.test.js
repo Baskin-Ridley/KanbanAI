a57831cef2f394f96e7c1395fa555b49a7d71705
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-23 15:29:03 +0000

    Update pages/AllBoards/index.test.js - Test complete

--- client/src/pages/AllBoards/index.test.js
+++ client/src/pages/AllBoards/index.test.js
@@ -2,27 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { UserContext } from '../../context/UserContext';
 import AllBoards from './index.jsx';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 
 describe('AllBoards', () => {
-    test('renders without error', async () => {
-        const mockUser = { id: 1 };
-        const mockBoards = [
-            { id: 1, title: 'Board 1' },
-            { id: 2, title: 'Board 2' },
-        ];
-        global.fetch = jest.fn().mockImplementation(() =>
-            Promise.resolve({
-                ok: true,
-                json: () => Promise.resolve(mockBoards),
-            })
-        );
-        render(
-            <Router>
-                <UserContext.Provider value={{ user: mockUser }}>
-                    <AllBoards />
-                </UserContext.Provider>
-            </Router>
-        );
+  test('renders without error', async () => {
+    const mockUser = { id: 1 };
+    const mockBoards = [
+      { id: 1, title: 'Board 1' },
+      { id: 2, title: 'Board 2' },
+    ];
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockBoards),
+        })
+      );
+    await act(async () => {
+      render(
+        <Router>
+          <UserContext.Provider value={{ user: mockUser }}>
+            <AllBoards />
+          </UserContext.Provider>
+        </Router>
+      );
     });
+  });
 });
