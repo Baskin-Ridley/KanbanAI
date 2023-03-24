a92f3524af3c995c14358ce09bf4ef7139a5f0b9
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-24 22:59:14 +0000

    Update components/StackCard/index.jsx. Refactor code.

--- client/src/components/StackCard/index.jsx
+++ client/src/components/StackCard/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import "./style.css"
 import { decode } from 'html-entities';
 
-function StackCard() {
+export const StackCard = () => {
 
     const [stack, setStack] = useState()
     const [title, setTitle] = useState("")
@@ -103,5 +103,3 @@ function StackCard() {
         </div>
     )
 }
-
-export default StackCard
