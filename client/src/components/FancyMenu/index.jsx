bf0c137a0cafa110710ee34c6c26bcec93d13f60
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-24 23:04:15 +0000

    Update components/FancyMenu/index.jsx. Move FancyMenu to components from pages.

---
+++ client/src/components/FancyMenu/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./style.css"
+import { FloatingMenu} from '../FloatingMenu'
+import { StackCard } from '../StackCard'
+import { GitCard } from '../GitCard'
+import { useView } from '../../context/UserContext'
+
+function FancyMenu() {
+    const { stackView, setStackView, gitView, setGitView } = useView()
+    return (
+        <main>
+            <FloatingMenu />
+            {stackView && <StackCard />}
+            {gitView && <GitCard />}
+        </main>
+    )
+}
+
+export default FancyMenu
