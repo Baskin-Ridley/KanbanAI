391ae87d541ed3b1fa26f9d552467d2d1986241e
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-24 23:00:41 +0000

    Update components/Header/index.jsx. Fix bug - update class to className.

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,11 +3,11 @@ import "../../index.css";
 
 const Header = () => (
   <header>
-    <div class="color-bg-0 py-4 md:flex md:justify-between md:items-center">
-      <div class="flex items-center justify-center md:justify-start px-4">
+    <div className="color-bg-0 py-4 md:flex md:justify-between md:items-center">
+      <div className="flex items-center justify-center md:justify-start px-4">
         <h1>Logo</h1>
       </div>
-      <div class="hidden md:flex items-center justify-center px-4">
+      <div className="hidden md:flex items-center justify-center px-4">
         <div className="color-bg-1 w-12 h-12 mr-2 transform scale-3"></div>
         <div className="color-bg-2 w-12 h-12 mr-2 transform scale-3"></div>
         <div className="color-bg-3 w-12 h-12 mr-2 transform scale-3"></div>
