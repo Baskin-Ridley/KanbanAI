951877e4cc2c1c1c0f094303eadcc712d5662d5f
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-24 09:21:48 +0000

    update buttons

--- client/src/components/AI/index.jsx
+++ client/src/components/AI/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Input, Button } from "..";
 const AISteps = () => {
   const [task, setTask] = useState("");
   const [responseData, setResponseData] = useState("");
@@ -22,13 +22,13 @@ const AISteps = () => {
   return (
     <div>
       <h1>AI Steps</h1>
-      <input
+      <Input
         type="text"
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
 
-      <button onClick={handleClick}>Click me</button>
+      <Button onClick={handleClick}>Click me</Button>
 
       {responseData && (
         <div>
