f03a797f00d6e79469421b3aa165fe332076cd22
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-24 15:12:58 +0000

    improve createticketpopup

--- client/src/components/Board/CreateTicketPopUp/index.jsx
+++ client/src/components/Board/CreateTicketPopUp/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const CreateTicketPopUp = () => {
-  return <div></div>;
+const CreateTicketPopUp = (props) => {
+  return (
+    <>
+      {props.isOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-white rounded-lg overflow-hidden shadow-xl">
+              <div className="p-4">Test</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CreateTicketPopUp;
