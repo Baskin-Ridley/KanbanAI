29806413dc756f6e9a70c112b6fd1f0055b186a5
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-24 22:19:16 +0000

    Update src.main.jsx. Import css only in src/index.html.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
