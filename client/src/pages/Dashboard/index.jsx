9bcf4dde953b973173241709fbf949d97295552a
shojib deb <shodeb123@gmail.com>
2023-03-25 00:28:44 +0000

    dashboard setup

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,13 +1,35 @@
 import React, { useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import "./dashboard.css"
+
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
   return (
-    <main className="w3-container w3-center">
-      <h2>Welcome, {user ? user.username : 'Guest'}!</h2>
-      <p>This is your dashboard.</p>
-    </main>
+    <>
+      <main className="w3-container w3-center">
+        <h2>Welcome, {user ? user.username : 'Guest'}!</h2>
+        <p>This is your dashboard.</p>
+      </main>
+
+      <div className='wrapper'>
+        <div className='row'>
+          <div className='col-4'>
+            <div className='left-container'>
+              <div className='Card'>
+                <p >peppe</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-8'>
+            <div className='right-container'>
+              <p>peppe</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 };
 
