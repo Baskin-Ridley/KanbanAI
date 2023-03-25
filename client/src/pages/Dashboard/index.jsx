4583e8f0cf3a6e3649ebb133e3e5d3bc5c08bb0f
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-25 04:47:17 +0000

    Update pages/Dashboard/index.jsx. CSS makeover 2 complete.

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -4,8 +4,8 @@ import { UserContext } from '../../context/UserContext';
 const Dashboard = () => {
   const { user } = useContext(UserContext);
   return (
-    <main className="w3-container w3-center">
-      <h2>Welcome, {user ? user.username : 'Guest'}!</h2>
+    <main className="flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-bold mb-8 m-12 text-center">Welcome, {user ? user.username : 'Guest'}!</h2>
       <p>This is your dashboard.</p>
     </main>
   );
