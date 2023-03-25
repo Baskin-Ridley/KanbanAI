5e8965d434a07d14022f8da6717dbf1e7004208f
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-25 04:51:41 +0000

    Update components/Form_Button/index.jsx. CSS makeover 2 complete.

--- client/src/components/Form_Button/index.jsx
+++ client/src/components/Form_Button/index.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const Form_Button = ({ buttonText, onClick, formElementId, ariaLabel }) => {
   return (
-    <button
-      className="color-bg-0 hover:color-bg-4 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      onClick={onClick}
-      id={formElementId}
-      aria-label={ariaLabel}
-      type="submit"
-    >
-      {buttonText}
-    </button>
+    <div className="flex flex-col items-center justify-center">
+      <button
+        className="color-bg-0 hover:color-bg-4 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={onClick}
+        id={formElementId}
+        aria-label={ariaLabel}
+        type="submit"
+        name={formElementId}
+      >
+        {buttonText}
+      </button>
+    </div>
   );
 };
 
