1bd2cef246e035fe5bd9108165917ff1d4d0463c
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-25 01:22:38 +0000

    Update pages/Login/index.jsx. CSS makeover.

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,8 +2,8 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
 import Message from "../../components/Message";
-import Button from "../../components/Button";
-import Input from "../../components/Input";
+import Form_Button from "../../components/Form_Button";
+import Form_Input from "../../components/Form_Input";
 import CustomLink from "../../components/CustomLink";
 
 const LoginPage = () => {
@@ -29,28 +29,30 @@ const LoginPage = () => {
       <Message message={error} type="error" />
       <form onSubmit={handleLogin}>
         <h2>Login</h2>
-        <Input
+        <Form_Input
           label="Username:"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          formElementId="login-field-username"
+          ariaLabel="Field in which to input the username"
         />
-        <Input
+        <Form_Input
           label="Password:"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          formElementId="login-field-password"
+          ariaLabel="Field in which to input the password"
         />
-        <Button type="submit">Login</Button>
+        <Form_Button buttonText="Login" formElementId="login-page-button-login" ariaLabel="Button for logging in" />
         <div className="mb-4">
           <span className="text-sm text-gray-600 mr-2">
             Don't have an account?
-          </span> <br />
-          <CustomLink
-            to="/register"
-          >
-            <Button type="submit">Register</Button>
-            {/* Register here. */}
+          </span>
+          <br />
+          <CustomLink to="/register">
+            <Button buttonText="Register" formElementId="login-page-button-register" ariaLogin="Button for registering" />
           </CustomLink>
         </div>
       </form>
