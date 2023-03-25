677827974be88106e3fad88cfe4992599a81c89f
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-25 01:22:06 +0000

    Update pages/Ticket/index.jsx. CSS makeover.

--- client/src/pages/Ticket/index.jsx
+++ client/src/pages/Ticket/index.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-import Textarea from "../../components/Textarea";
+import Form_Button from "../../components/Form_Button";
+import Form_Input from "../../components/Form_Input";
+import Form_Textarea from "../../components/Form_Textarea";
 
 const Ticket = () => {
     const [technologies, setTechnologies] = useState('Python');
@@ -45,78 +44,56 @@ const Ticket = () => {
         <main className="form-container">
             <h2>Create New Ticket</h2>
             <form onSubmit={handleFormSubmit}>
-                {/* <div>
-                    <label htmlFor="title">Title</label><br />
-                    <input type="text" id="title" value="Sample Title" readOnly />
-                </div> */}
-                <Input
+                <Form_Input
                     label="Title:"
                     type="text"
-                    id="title"
                     value="Sample Title"
+                    formElementId="title"
+                    ariaLabel="Field in which the ticket title is displayed"
                 />
-                {/* <div>
-                    <label htmlFor="content">Content</label><br />
-                    <textarea id="content" value="Sample Content" readOnly />
-                </div> */}
-                <Input
+                <Form_Input
                     label="Content:"
                     type="text"
-                    id="content"
                     value="Sample Content"
+                    formElementId="content"
+                    ariaLabel="Field in which the ticket content is displayed"
                 />
-                {/* <div>
-                    <label htmlFor="user">User</label><br />
-                    <input type="text" id="user" value="Anonymous" readOnly />
-                </div> */}
-                <Input
+                <Form_Input
                     label="User:"
                     type="text"
-                    id="user"
                     value="User 1"
+                    formElementId="user"
+                    ariaLabel="Field in which the user is displayed"
                 />
-                {/* <div>
-                    <label htmlFor="technologies">Technologies</label><br />
-                    <input type="text" id="technologies" value={technologies} onChange={(e) => setTechnologies(e.target.value)} />
-                </div> */}
-                <Input
+                <Form_Input
                     label="Technologies:"
                     type="text"
-                    id="technologies"
                     value="python"
+                    formElementId="technologies"
+                    ariaLabel="Field for input of technologies"
                 />
-                {/* <div>
-                    <label htmlFor="test-framework">Test Framework</label><br />
-                    <input type="text" id="test-framework" value={testFramework} onChange={(e) => setTestFramework(e.target.value)} />
-                </div> */}
-                <Input
+                <Form_Input
                     label="Test Framework:"
                     type="text"
-                    id="test-framework"
                     value="pytest"
+                    formElementId="test-framework"
+                    ariaLabel="Field for input of test framework"
                 />
-                {/* <div>
-                    <label htmlFor="function-to-test">Function to Test</label><br />
-                    <textarea rows="4" cols="50" id="function-to-test" value={functionToTest} onChange={(e) => setFunctionToTest(e.target.value)} />
-                </div> */}
-                <Textarea
+                <Form_Textarea
                     label="Function to Test:"
-                    onChange={(e) => setFunctionToTest(e.target.value)}
-                    id="function-to-test"
                     value={functionToTest}
+                    onChange={(e) => setFunctionToTest(e.target.value)}
+                    formElementId="function-to-test"
+                    ariaLabel="Textarea in which to write the function to test"
                 />
-                {/* <div>
-                    <label htmlFor="tests-for-function">Tests for Function</label><br />
-                    <textarea rows="4" cols="50" id="tests-for-function" value={testsForFunction} onChange={(e) => setTestsForFunction(e.target.value)} />
-                </div> */}
-                <Textarea
+                <Form_Textarea
                     label="Tests for Function:"
-                    onChange={(e) => setTestsForFunction(e.target.value)}
-                    id="tests-for-function"
                     value={testsForFunction}
+                    onChange={(e) => setTestsForFunction(e.target.value)}
+                    formElementId="tests-for-function"
+                    ariaLabel="Textarea in which generated tests are displayed"
                 />
-                {/* <button type="submit">Generate Tests</button> */}
-                <Button type="submit">Generate Tests</Button>
+                <Form_Button buttonText="Generate tests" formElementId="button-generate-test" ariaLabel="Button for generating tests" />
             </form>
         </main>
     );
