ed0899a560420dcd5d45cc8a01959e5b65bfd1ab
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-25 04:51:06 +0000

    Update components/Form_Label/index.jsx. CSS makeover 2 complete.

--- client/src/components/Form_Label/index.jsx
+++ client/src/components/Form_Label/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Form_Label = ({ label, formElementId }) => {
 
   return (
-    <div className="mb-4">
+    <div className="mb-4 text-center ">
       <label className="block color-text-4 font-bold mb-2" htmlFor={formElementId}>{label}</label>
     </div>
   );
