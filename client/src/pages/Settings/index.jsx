9458a56adb6cc214c66f730d87bafe7a826fb3d4
shojib deb <shodeb123@gmail.com>
2023-03-27 00:11:35 +0100

    setup setting

--- client/src/pages/Settings/index.jsx
+++ client/src/pages/Settings/index.jsx
@@ -17,19 +17,21 @@ const Settings = () => {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-bold mb-8 m-12 text-center">Settings</h2>
-      {message && <Message>{message}</Message>}
-      <form className="w-full max-w-md" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <Form_Input label="New Password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} formElementId="password" ariaLabel="Field for inputting the password" />
-        </div>
-        <div className="mb-4">
-          <Form_Input label="New Email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} formElementId="email" ariaLabel="Field for inputting the email" />
-        </div>
-        <Form_Button buttonText="Update" ariaLabel="Button for submitting the form" />
-      </form>
-    </main>
+    <>
+      <main className="flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold mb-8 m-12 text-center">Settings</h2>
+        {message && <Message>{message}</Message>}
+        <form className="w-full max-w-md" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <Form_Input label="New Password" type="password" value={password} onChange={(event) => setPassword(event.target.value)} formElementId="password" ariaLabel="Field for inputting the password" />
+          </div>
+          <div className="mb-4">
+            <Form_Input label="New Email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} formElementId="email" ariaLabel="Field for inputting the email" />
+          </div>
+          <Form_Button buttonText="Update" ariaLabel="Button for submitting the form" />
+        </form>
+      </main>
+    </>
   );
 };
 
