91a578b46a2a1e877aaa755fe8e1438e18928186
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-28 14:45:42 +0100

    update link to scroll to top

--- client/src/pages/b2b/components/Hook.jsx
+++ client/src/pages/b2b/components/Hook.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hook = () => {
+  function handleClick() {
+    window.scrollTo(window.scrollTo(0, 0));
+  }
+
   return (
     <div className="bg-gradient-to-br from-indigo-500 to-[#86AFB1] shadow-lg p-8 max-w mx-auto text-center">
       <h2 className="text-3xl font-bold text-white mb-6">What is Kanban AI?</h2>
@@ -9,9 +14,13 @@ const Hook = () => {
         would expect but with a little more magic... Each feature is powerful on
         its own but when supercharged by AI they become truly groundbreaking.
       </p>
-      <button className="bg-white text-blue-500 py-2 px-4 rounded-lg mt-6 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
+      <Link
+        to="/register"
+        onClick={handleClick}
+        className="bg-white text-blue-500 py-2 px-4 rounded-lg mt-6 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out animate-shake"
+      >
         Start Free!
-      </button>
+      </Link>
     </div>
   );
 };
