4e75a08a6de8f046effde595fe9bec6539ed286e
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-28 17:02:54 +0100

    fix Hook

--- client/src/pages/b2b/components/Hook.jsx
+++ client/src/pages/b2b/components/Hook.jsx
@@ -7,8 +7,10 @@ const Hook = () => {
   }
 
   return (
-    <div className="bg-gradient-to-br from-indigo-500 to-[#86AFB1] shadow-lg p-8 max-w mx-auto text-center">
-      <h2 className="text-3xl font-bold text-white mb-6">What is Kanban AI?</h2>
+    <div className="flex flex-col justify-center align-middle bg-gradient-to-br from-indigo-500 to-[#86AFB1] shadow-lg p-8 max-w mx-auto text-center h-96">
+      <h2 className="flex text-3xl font-bold text-white mb-6 justify-center align-middle">
+        What is Kanban AI?
+      </h2>
       <p className="text-white leading-8">
         Kanban AI is a project management solution with all the features you
         would expect but with a little more magic... Each feature is powerful on
@@ -17,7 +19,7 @@ const Hook = () => {
       <Link
         to="/register"
         onClick={handleClick}
-        className="bg-white text-blue-500 py-2 px-4 rounded-lg mt-6 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out animate-shake"
+        className="bg-white w-36 text-blue-500 py-2 px-4 rounded-lg mt-6 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out animate-shake mx-auto p-4"
       >
         Start Free!
       </Link>
