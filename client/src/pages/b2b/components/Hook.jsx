905775f87dbca377344d5bf1c4333796a2276022
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-28 14:27:32 +0100

    change colors

--- client/src/pages/b2b/components/Hook.jsx
+++ client/src/pages/b2b/components/Hook.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Hook = () => {
   return (
-    <div className="bg-gradient-to-br from-pink-500 to-purple-500 shadow-lg p-8 max-w mx-auto text-center">
+    <div className="bg-gradient-to-br from-indigo-500 to-[#86AFB1] shadow-lg p-8 max-w mx-auto text-center">
       <h2 className="text-3xl font-bold text-white mb-6">What is Kanban AI?</h2>
       <p className="text-white leading-8">
         Kanban AI is a project management solution with all the features you
         would expect but with a little more magic... Each feature is powerful on
         its own but when supercharged by AI they become truly groundbreaking.
       </p>
-      <button className="bg-white text-pink-500 py-2 px-4 rounded-lg mt-6 hover:bg-pink-500 hover:text-white transition duration-300 ease-in-out">
+      <button className="bg-white text-blue-500 py-2 px-4 rounded-lg mt-6 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
         Start Free!
       </button>
     </div>
