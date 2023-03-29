a970b0e7a8aa62ae791b7ef106afa0a20d99b5e4
kay-dev-uk <kay-dev-uk@gmail.com>
2023-03-29 09:53:38 +0100

    add some styling

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="p-2 text-center text-xs w-full bg-gray-800 text-white fixed bottom-0">
-    <p>Kanban DevTest App. Created by Kay, Gabriel, Sho and Gabrielle. License: MIT.</p>
+    <p>Kanban AI. Created by Kay, Gabriel, Sho and Gabrielle. License: MIT.</p>
   </footer>
 );
 
