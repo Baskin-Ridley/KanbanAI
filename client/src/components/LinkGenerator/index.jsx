edba440533482bf9011bed23ec161375d49af4de
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-30 09:52:13 +0100

    green > blue

--- client/src/components/LinkGenerator/index.jsx
+++ client/src/components/LinkGenerator/index.jsx
@@ -1,45 +1,43 @@
-import { useState } from 'react';
-import { useView } from '../../context/UserContext';
+import { useState } from "react";
+import { useView } from "../../context/UserContext";
 
 function LinkGenerator() {
-    const [link, setLink] = useState('');
-    const [copied, setCopied] = useState(false);
-    const { user } = useView()
+  const [link, setLink] = useState("");
+  const [copied, setCopied] = useState(false);
+  const { user } = useView();
 
-    const handleClick = () => {
-        // Generate your link here, e.g.:
-        const generatedLink = `https://localhost:5000/register/${user.username}`;
+  const handleClick = () => {
+    // Generate your link here, e.g.:
+    const generatedLink = `https://localhost:5000/register/${user.username}`;
 
-        // Set the link in state
-        setLink(generatedLink);
-    };
+    // Set the link in state
+    setLink(generatedLink);
+  };
 
-    const handleCopy = () => {
-        // Copy the link to the clipboard
-        navigator.clipboard.writeText(link);
+  const handleCopy = () => {
+    // Copy the link to the clipboard
+    navigator.clipboard.writeText(link);
 
-        // Set copied state to true and reset after 2 seconds
-        setCopied(true);
-        setTimeout(() => {
-            setCopied(false);
-        }, 2000);
-    };
+    // Set copied state to true and reset after 2 seconds
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
 
-    return (
-        <div className='link-generator-wrapper'>
-            <button onClick={handleClick}>Generate Link</button>
-            {link && (
-                <div>
-                    <input type="text" value={link} readOnly />
-                    <br />
-                    <button onClick={handleCopy}>Copy Link</button>
-                </div>
-            )}
-            {copied && (
-                <div style={{ color: 'green' }}>Copied to clipboard!</div>
-            )}
+  return (
+    <div className="link-generator-wrapper">
+      <button onClick={handleClick}>Generate Link</button>
+      {link && (
+        <div>
+          <input type="text" value={link} readOnly />
+          <br />
+          <button onClick={handleCopy}>Copy Link</button>
         </div>
-    );
+      )}
+      {copied && <div style={{ color: "darkblue" }}>Copied to clipboard!</div>}
+    </div>
+  );
 }
 
 export default LinkGenerator;
