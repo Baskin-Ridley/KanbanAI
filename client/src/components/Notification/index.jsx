3a14752b238bbf1d405e13c2f24d986469c15de6
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-30 10:36:02 +0100

    solve conflicts

--- client/src/components/Notification/index.jsx
+++ client/src/components/Notification/index.jsx
@@ -17,44 +17,28 @@ const NotificationCard = () => {
     fetchNotifications();
   }, []);
 
-<<<<<<< HEAD
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+      />
       {notification && (
         <div className="flex items-center justify-center mx-auto m-2 rounded-lg border border-gray-400 bg-blue-50">
-          <h2 className="text-3xl font-bold m-2 text-center">Notifications</h2>
+          <h2 className="text-3xl font-bold m-2 text-center">
+            <i className="fa fa-comments-o" aria-hidden="true"></i>
+            <i className="fa fa-exclamation" aria-hidden="true"></i>
+            Notifications
+          </h2>
           <div className="outerbox min-h-20 rounded-lg border-dashed border-transparent bg-blue-100 p-2 transition-colors duration-150 hover:border-gray-400 hover:bg-blue-200 w-full">
             <ul className="wrapper-notifications">
               {notification.map((e, i) => (
                 <div
-                  className="wrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false"
+                  className="wrap border flex justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white"
                   key={i}
                 >
-                  <li className="w-full"> {e.content}</li>
+                  <li className="w-full">{e.content}</li>
                   <li className="w-full">By User: {e.member}</li>
-=======
-        fetchNotifications()
-    }, [])
-
-
-
-    return (
-        <>
-            <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-            {notification &&
-                <div className="flex items-center justify-center mx-auto m-2 rounded-lg border border-gray-400 bg-blue-50">
-                    <h2 className="text-3xl font-bold m-2 text-center"><i class="fa fa-comments-o" aria-hidden="true"></i><i class="fa fa-exclamation" aria-hidden="true"></i>Notifications</h2>
-                    <div className='outerbox min-h-20 rounded-lg border-dashed border-transparent bg-blue-100 p-2 transition-colors duration-150 hover:border-gray-400 hover:bg-blue-200 w-full'>
-                        <ul className="wrapper-notifications">
-                            {notification.map((e, i) => (
-                                <div className="wrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false" key={i}>
-                                    <li className="w-full"> {e.content}</li>
-                                    <li className="w-full">By User: {e.member}</li>
-                                </div>
-                            ))}
-                        </ul>
-                    </div>
->>>>>>> cb3c26470b2901ad496e550642f2087074c179b2
                 </div>
               ))}
             </ul>
