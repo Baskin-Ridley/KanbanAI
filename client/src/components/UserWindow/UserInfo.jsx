ff02ffcfceabc718f6749c0e00f94c24ed5dd788
shojib deb <shodeb123@gmail.com>
2023-03-30 01:42:41 +0100

    fixed send email from dashboard

--- client/src/components/UserWindow/UserInfo.jsx
+++ client/src/components/UserWindow/UserInfo.jsx
@@ -4,10 +4,21 @@ const UserInfo = ({ selectedUser, handleBackClick }) => {
     const { avatar, email } = selectedUser
     const [body, setBody] = useState("")
 
+    const handleChange = (e) => {
+        setBody(e.target.value)
+        console.log(e.target.value)
+    }
 
     const handlesendClick = async () => {
 
-        const response = await fetch("http://localhost:5000/email")
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ body: body, email: email })
+        };
+
+        const response = await fetch("http://localhost:5000/dashboard/email", requestOptions)
         const data = await response.json()
         console.log(data)
 
@@ -20,7 +31,7 @@ const UserInfo = ({ selectedUser, handleBackClick }) => {
                 <div className=' email-fieldwrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false '>Email: {email}</div>
                 <div>
                     <textarea className='input-box px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full h-full 
-                    '  cols="40" rows="5" type="text" placeholder="Enter your input here" />
+                    '  cols="40" rows="5" type="text" placeholder="Enter your input here" onChange={(e) => { handleChange(e) }} />
                 </div>
                 <button className='send-button bg-blue-500 hover:color-bg-4 focus:shadow-outline rounded py-2 px-4 font-bold text-white focus:outline-none hover:bg-blue-200 hover:text-black undefined w-full' onClick={handlesendClick}>Send</button>
             </div>
--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, session, render_template
 from database import db
 from models import User
 import openai
-from controllers import register_user, login, find_user_by_username, get_users, get_user, update_user, delete_user, create_kanban_ticket, get_kanban_tickets, get_kanban_ticket, update_kanban_ticket, delete_kanban_ticket, create_kanban_board, get_kanban_board, get_kanban_boards, update_kanban_board, delete_kanban_board, get_kanban_tickets_by_board, create_kanban_header, register_Super_User, get_kanban_headers_by_board, delete_kanban_header_by_board, get_Notifications, add_member, get_positions_by_board, update_positions_by_board, log_changes, get_members, update_kanban_headers_by_board, get_kanban_boards_with_super, email_from_form
+from controllers import register_user, login, find_user_by_username, get_users, get_user, update_user, delete_user, create_kanban_ticket, get_kanban_tickets, get_kanban_ticket, update_kanban_ticket, delete_kanban_ticket, create_kanban_board, get_kanban_board, get_kanban_boards, update_kanban_board, delete_kanban_board, get_kanban_tickets_by_board, create_kanban_header, register_Super_User, get_kanban_headers_by_board, delete_kanban_header_by_board, get_Notifications, add_member, get_positions_by_board, update_positions_by_board, log_changes, get_members, update_kanban_headers_by_board, get_kanban_boards_with_super, email_from_form,email_from_dashboard
 # create_user
 
 
@@ -265,6 +265,9 @@ def update_positions_by_board_route(kanban_board_id):
 def send_email():
     return email_from_form()
 
+@app.route("/dashboard/email", methods=['POST'])
+def send_email_dashbaord():
+    return email_from_dashboard()
 
 if __name__ == '__main__':
     app.run()
