08047a72cdeab032f678af12032fe3a08db55490
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-31 09:33:40 +0100

    update user

--- client/src/components/UserWindow/UserInfo.jsx
+++ client/src/components/UserWindow/UserInfo.jsx
@@ -1,44 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const UserInfo = ({ selectedUser, handleBackClick }) => {
-    const { avatar, email } = selectedUser
-    const [body, setBody] = useState("")
+  const { avatar, email } = selectedUser;
+  const [body, setBody] = useState("");
 
-    const handleChange = (e) => {
-        setBody(e.target.value)
-        console.log(e.target.value)
-    }
+  const handleChange = (e) => {
+    setBody(e.target.value);
+    console.log(e.target.value);
+  };
 
-    const handlesendClick = async () => {
+  const handlesendClick = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ body: body, email: email }),
+    };
 
+    const response = await fetch(
+      "http://localhost:5000/dashboard/email",
+      requestOptions
+    );
+    const data = await response.json();
+    console.log(data);
 
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ body: body, email: email })
-        };
+    setBody("");
+  };
 
-        const response = await fetch("http://localhost:5000/dashboard/email", requestOptions)
-        const data = await response.json()
-        console.log(data)
-
-        setBody('')
-
-    }
-
-    return (
-        <div clasName="wrapper-textbox" style={{ display: 'flex' }}>
-            <img className='photo border ' src={avatar} alt="user avatar" style={{ width: 100, height: 100 }} />
-            <div style={{ marginLeft: 20 }}>
-                <div className=' email-fieldwrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false w-full'>Email: {email}</div>
-                <div>
-                    <textarea className='input-box px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-fill h-full 
-                    '  cols="40" rows="5" type="text" placeholder="Enter your input here" onChange={(e) => { handleChange(e) }} />
-                </div>
-                <button className='send-button bg-blue-500 hover:color-bg-4 focus:shadow-outline rounded py-2 px-4 font-bold text-white focus:outline-none hover:bg-blue-200 hover:text-black undefined w-full' onClick={handlesendClick}>Send</button>
-            </div>
+  return (
+    <div clasName="wrapper-textbox" style={{ display: "flex" }}>
+      <img
+        className="photo border "
+        src={avatar}
+        alt="user avatar"
+        style={{ width: 100, height: 100 }}
+      />
+      <div style={{ marginLeft: 20 }}>
+        <div className=" email-fieldwrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false w-full">
+          Email: {email}
         </div>
-    )
-}
+        <div>
+          <textarea
+            className="input-box px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-fill h-full 
+                    "
+            cols="40"
+            rows="5"
+            type="text"
+            placeholder="Enter your input here"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <button
+          className="send-button bg-blue-500 hover:color-bg-4 focus:shadow-outline rounded py-2 px-4 font-bold text-white focus:outline-none hover:bg-blue-200 hover:text-black undefined w-full"
+          onClick={handlesendClick}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
--- client/src/components/UserWindow/UserList.jsx
+++ client/src/components/UserWindow/UserList.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const UserList = ({ userData, handleUserClick }) => {
-    return (
-        <div>
-            {userData.map((user) => (
-                <div className='wrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false'>
-                    <div key={user.username} onClick={() => handleUserClick(user)} style={{ cursor: 'pointer' }}>
-                        <img src={user.avatar} alt="user avatar" style={{ width: 50, height: 50 }} />
-                        <div style={{ display: 'inline-block', marginLeft: 20 }}>{user.username}</div>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div>
+      {userData.map((user) => (
+        <div className="wrap border flex  justify-center mx-auto m-2 rounded-lg border-gray-400 bg-blue-50 mb-2 rounded-md bg-blue-50 py-2 px-4 text-sm text-center shadow-md border-gray-400 transition-colors duration-150 hover:bg-blue-300 hover:text-white false">
+          <div
+            key={user.username}
+            onClick={() => handleUserClick(user)}
+            style={{ cursor: "pointer" }}
+          >
+            <img
+              src={user.avatar}
+              alt="user avatar"
+              style={{ width: 50, height: 50 }}
+            />
+            <div style={{ display: "inline-block" }}>{user.username}</div>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default UserList
+export default UserList;
